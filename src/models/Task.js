3eba7ef2391784cf5ff0d843bf6da180b6a9b7bc
= <tonygithinji94@gmail.com>
2019-11-29 20:48:34 +0300

    Task addition updates

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const schema = mongoose.Schema({
-    name: { type: String, required: true },
+    task: { type: String, required: true },
     complete: { type: Boolean, default: false },
     projectId: { type: String, required: true },
     workspaceId: { type: String, required: true },
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,27 +1,30 @@
 import express from "express";
 import Task from "../models/Task";
+import Project from "../models/Project";
 import authenticate from "../middleware/authenticated";
 
 const router = express.Router();
 router.use(authenticate);
 
 router.post("/", (req, res) => {
-    const { name, projectId, workspaceId } = req.body.data;
-    const task = new Task();
-    task.name = name;
-    task.projectId = projectId;
-    task.workspaceId = workspaceId;
-    task.save()
-        .then((newTask) => res.json({ status: "ok", task: newTask }))
+    const { task, projectId, workspaceId } = req.body.data;
+    const newTask = new Task();
+    newTask.task = task;
+    newTask.projectId = projectId;
+    newTask.workspaceId = workspaceId;
+    newTask.save()
+        .then((createdTask) => res.json({ status: "ok", task: createdTask }))
         .catch(() => res.status(400).json({ errors: { global: "An unexpected error occurred" } }));
 });
 
 router.get("/:projectId", (req, res) => {
     const projectId = req.params.projectId;
 
-    Task.find({ projectId })
-        .limit(10)
-        .then(tasks => res.json({ status: "ok", tasks }));
+    const projectPromise = Project.findById(projectId);
+    const tasksPromise = Task.find({ projectId }).limit(10);
+
+    Promise.all([projectPromise, tasksPromise])
+        .then(([project, tasks]) => res.json({ status: "ok", tasks, project }));
 });
 
 export default router;
