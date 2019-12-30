28d917c8757fe96ba437b53b85bc4dadc95ccab3
Anthony Githinji <tonygithinji94@gmail.com>
2019-12-30 12:06:59 +0300

    Toggling a task's complete status

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -7,6 +7,7 @@ import authenticate from "../middleware/authenticated";
 const router = express.Router();
 router.use(authenticate);
 
+// Create a new task
 router.post("/", (req, res) => {
     const { task, projectId, workspaceId } = req.body.data;
     const newTask = new Task();
@@ -15,8 +16,8 @@ router.post("/", (req, res) => {
     newTask.workspaceId = workspaceId;
     newTask.save()
         .then((createdTask) => {
-            const projectPromise = Project.findByIdAndUpdate(projectId).update({ $inc: { tasksNumber: 1 } });
-            const workspacePromise = Workspace.findByIdAndUpdate(workspaceId).update({ $inc: { tasksNumber: 1 } });
+            const projectPromise = Project.findByIdAndUpdate(projectId, { $inc: { tasksNumber: 1 } });
+            const workspacePromise = Workspace.findByIdAndUpdate(workspaceId, { $inc: { tasksNumber: 1 } });
 
             Promise.all([projectPromise, workspacePromise])
                 .then(() => res.json({ status: "ok", task: createdTask }))
@@ -25,6 +26,7 @@ router.post("/", (req, res) => {
         .catch(() => res.status(400).json({ errors: { global: "An unexpected error occurred" } }));
 });
 
+// Fetch all tasks for a particular project
 router.get("/:projectId", (req, res) => {
     const projectId = req.params.projectId;
 
@@ -35,6 +37,7 @@ router.get("/:projectId", (req, res) => {
         .then(([project, tasks]) => res.json({ status: "ok", tasks, project }));
 });
 
+// Update a task
 router.post("/update", (req, res) => {
     const { task, taskId } = req.body.data;
     Task.findByIdAndUpdate(taskId, { task }, { new: true })
@@ -42,12 +45,29 @@ router.post("/update", (req, res) => {
         .catch(() => res.status(400).json({ errors: { global: "An unexpected error occurred" } }));
 });
 
+// Delete a task
 router.post("/delete", (req, res) => {
     const { taskId, projectId, workspaceId } = req.body.data;
     Task.findByIdAndDelete(taskId)
         .then(() => {
-            const projectPromise = Project.findByIdAndUpdate(projectId).update({ $inc: { tasksNumber: -1 } });
-            const workspacePromise = Workspace.findByIdAndUpdate(workspaceId).update({ $inc: { tasksNumber: -1 } });
+            const projectPromise = Project.findByIdAndUpdate(projectId, { $inc: { tasksNumber: -1 } });
+            const workspacePromise = Workspace.findByIdAndUpdate(workspaceId, { $inc: { tasksNumber: -1 } });
+
+            Promise.all([projectPromise, workspacePromise])
+                .then(() => res.json({ status: "ok" }))
+                .catch(() => res.status(400).json({ errors: { global: "An unexpected error occurred" } }));
+        })
+        .catch(() => res.status(400).json({ errors: { global: "An unexpected error occurred" } }));
+});
+
+// Toggle a task's complete status
+router.post("/complete", (req, res) => {
+    const { taskId, projectId, workspaceId, status } = req.body.data;
+    Task.findByIdAndUpdate(taskId, { complete: status })
+        .then(() => {
+            const updateCount = status ? 1 : -1;
+            const projectPromise = Project.findByIdAndUpdate(projectId, { $inc: { completedTasks: updateCount } });
+            const workspacePromise = Workspace.findByIdAndUpdate(workspaceId, { $inc: { completedTasks: updateCount } });
 
             Promise.all([projectPromise, workspacePromise])
                 .then(() => res.json({ status: "ok" }))
